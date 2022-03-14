982307c658ee1a466cdd074b3fa1787528901b86
onlineceoco <online.ceo.product@gmail.com>
2022-03-14 20:22:21 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = phase => {
         domains: ["www.esafety.gov.au", "localhost", "api.vetnow.ir"],
       },
       env: {
-        api: "http://localhost:8001/api/v1/",
+        api: "",
         baseUrl: "http://localhost:8001",
       },
     };
@@ -18,7 +18,7 @@ module.exports = phase => {
       domains: ["www.esafety.gov.au", "api.vetnow.ir"],
     },
     env: {
-      api: "https://api.vetnow.ir/api/v1/",
+      api: "",
       baseUrl: "https://api.vetnow.ir",
     },
   };
